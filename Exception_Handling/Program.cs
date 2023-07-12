bcc0ece1bcce8b393570aa38837ca99c2efbc211
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-12 13:50:45 +0530

    Add project files.

---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Exception_Handling.Student;
+
+namespace Exception_Handling
+{
+    public class Program
+    {
+       
+
+
+        static void Main(string[] args)
+        {
+          Student stud = new Student();
+            try
+            {
+                stud.AcceptDetails("  ", 17);
+
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                stud.AcceptDetails("", 18);
+            }
+            catch(NameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Exception_Handling/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Student
+    {
+        public class AgeException : Exception
+        {
+            public AgeException(string message) : base(message)
+            {
+            }
+        }
+        public class NameException : Exception
+        {
+            public NameException(string message) : base(message)
+            {
+
+            }
+        }
+        private string name;
+        private int age;
+
+        public void AcceptDetails(string name, int age)
+        {
+            if (age < 18)
+            {
+                throw new AgeException("Your age is not valid");
+            }
+            if (string.IsNullOrEmpty(name))
+            {
+               throw new NameException("Please mention name");
+            }
+            else
+            {
+                this.name = name;
+                this.age = age;
+            }
+        }
+    }
+}
